94948c79654880ac5f223d80397c258fd0d2ad2c
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-21 15:13:04 +0200

    Add files via upload

---
+++ Book 1/Chapter_2/Tryouts p17/tryouts.js
@@ -0,0 +1,18 @@
+// Variable initialization:
+// Creating a variable named 'balloons' and storing the number 15
+var balloons = 15; 
+
+// Creating a second variable named 'moreballoons' and storing the number 9
+var moreballoons = 9; 
+
+// Note: In a browser console, the line above returns 'undefined' 
+// simply because assigning a value to a variable doesn't have a "result" to show.
+undefined
+
+// Mathematical Expression:
+// Step 1: Parentheses () force the addition to happen first (15 + 9 = 24)
+// Step 2: The result (24) is then multiplied by 2
+(balloons + moreballoons) * 2;
+
+// The console outputs 48, which is the final calculated value.
+48
