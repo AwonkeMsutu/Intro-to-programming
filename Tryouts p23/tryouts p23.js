f95627d89e79808e4c5c53ebaad5f4e4fbfdc21d
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-25 16:35:08 +0200

    Delete Tryouts p23 directory

--- Tryouts p23/tryouts p23.js
+++
@@ -1,9 +0,0 @@
-// Initialize the variable 'balloons' with a value of 100
-var balloons = 100; 
-
-// The '*=' operator multiplies the current value by the number on the right
-// This is shorthand for: balloons = balloons * 2;
-balloons *= 2;
-
-// The result is now 200
-console.log(balloons);
