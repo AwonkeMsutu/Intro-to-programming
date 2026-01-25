c60d020b7a3edf8dd835872a307b729638900a78
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-25 16:33:44 +0200

    Add files via upload

---
+++ Book 1/Chapter_2/Tryouts p37/tryouts p37.js
@@ -0,0 +1,14 @@
+// Set the visitor's age
+var age = 12;
+
+// Set whether they are with an adult (true/false)
+var accompanied = true;
+
+// The OR (||) operator checks if either condition is true:
+// 1. Is the age 13 or older? 
+// 2. OR is accompanied equal to true?
+if (age >= 13 || accompanied === true)
+{
+    // If either condition is met, this message prints to the console
+    console.log("You may enter theatre");
+}
