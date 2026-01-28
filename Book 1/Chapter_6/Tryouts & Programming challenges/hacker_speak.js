91bded74d3381ebb9aac5762a3944ec06e64aed2
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-28 16:37:15 +0200

    Update hacker_speak.js

--- Book 1/Chapter_6/Tryouts & Programming challenges/hacker_speak.js
+++ Book 1/Chapter_6/Tryouts & Programming challenges/hacker_speak.js
@@ -3,23 +3,23 @@ var output = "";
 for(var i = 0; i < input.length; i++)
 {
     var character = input[i];
-}
-if(character === a)
+
+if(character === "a")
 {
     output += "4";
 }
-else if (character === e)
+else if (character === "e")
 {
     output +="3";
 }
-else if (character === i)
+else if (character === "i")
 {
     output += "1"
 }
-else  if (character == o)
+else  if (character === "o")
 {
     output += "0"
 }
 else {output +=character}
-
+}
 console.log(output);
