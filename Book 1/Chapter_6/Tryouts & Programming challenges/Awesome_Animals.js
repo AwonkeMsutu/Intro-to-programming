900dc37f69598ec429dcad5c010965f8397a5ee6
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-28 09:24:19 +0200

    Add files via upload

---
+++ Book 1/Chapter_6/Tryouts & Programming challenges/Awesome_Animals.js
@@ -0,0 +1,14 @@
+// 1. We create an array (a list) of animal strings
+var Animals = ["Cat", "Fish", "Lemur", "Komodo Dragon"];
+
+// 2. We start a 'for' loop:
+//    - 'i = 0': start at the first item (index 0)
+//    - 'i < 4': keep going as long as i is less than the number of items
+//    - 'i++': add 1 to i after every loop
+for(var i = 0; i < 4; i++)
+{
+    // 3. This prints the word "Awesome " followed by the animal at the current position [i]
+    //    It doesn't change the list, it just "shouts" the result into the console.
+    console.log("Awesome " + Animals[i]);
+}
+//How to ensure that values are reassigned to original array in order to modify and not just print "Awesome"??
---
+++ Book 1/Chapter_6/Tryouts & Programming challenges/Random_string.js
@@ -0,0 +1,8 @@
+var Alphabet = "abcdefghijklmnopqrstuvwxyz"
+Math.floor(Math.random() * Alphabet.length);
+var randomstring = ("") 
+while(randomstring.length < 6)
+{
+    var randomIndex = Math.floor(Math.random() * Alphabet.length)
+}
+console.log(randomstring);
