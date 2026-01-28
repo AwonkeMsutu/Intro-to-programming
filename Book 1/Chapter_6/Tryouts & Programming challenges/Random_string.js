0b8aeeab19cecf4a6dfdfe53a33a7ebf91f5d8ff
AwonkeMsutu <apecsrealish@gmail.com>
2026-01-28 16:26:35 +0200

    Update Random_string.js

--- Book 1/Chapter_6/Tryouts & Programming challenges/Random_string.js
+++ Book 1/Chapter_6/Tryouts & Programming challenges/Random_string.js
@@ -4,5 +4,11 @@ var randomstring = ("")
 while(randomstring.length < 6)
 {
     var randomIndex = Math.floor(Math.random() * Alphabet.length)
+
+    // Get the random letter at that index
+    var randomLetter = Alphabet[randomIndex];
+
+    // Add the letter to the end of the string
+    randomstring += randomLetter;
 }
 console.log(randomstring);
